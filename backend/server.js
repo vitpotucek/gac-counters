989a1ac36c5e612e729cc48ccc031df5ce5bee10
vitpotucek <9y6rp8wg5k@privaterelay.appleid.com>
2026-04-28 10:51:12 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs";
 import cors from "cors";
+import { nanoid } from "nanoid";
 
 const app = express();
 app.use(cors());
@@ -8,34 +9,58 @@ app.use(express.json());
 
 const DATA_FILE = "./data.json";
 
+function readData() {
+  return JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
+}
+
+function writeData(data) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+}
+
 // GET all counters
 app.get("/counters", (req, res) => {
-  const data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
-  res.json(data);
+  res.json(readData());
 });
 
 // ADD new counter
 app.post("/counters", (req, res) => {
-  const newCounter = req.body;
-
-  const data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
+  const newCounter = { id: nanoid(), ...req.body };
+  const data = readData();
   data.push(newCounter);
-
-  fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
-
-  res.json({ success: true, added: newCounter });
+  writeData(data);
+  res.json({ success: true, counter: newCounter });
 });
 
-// DELETE counter by index
+// UPDATE counter
+app.put("/counters/:id", (req, res) => {
+  const { id } = req.params;
+  const data = readData();
+  const index = data.findIndex(c => c.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ success: false, message: "Not found" });
+  }
+
+  data[index] = { ...data[index], ...req.body };
+  writeData(data);
+
+  res.json({ success: true, counter: data[index] });
+});
+
+// DELETE counter
 app.delete("/counters/:id", (req, res) => {
-  const id = Number(req.params.id);
+  const { id } = req.params;
+  const data = readData();
+  const filtered = data.filter(c => c.id !== id);
 
-  const data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
-  const updated = data.filter((_, i) => i !== id);
+  if (filtered.length === data.length) {
+    return res.status(404).json({ success: false, message: "Not found" });
+  }
 
-  fs.writeFileSync(DATA_FILE, JSON.stringify(updated, null, 2));
-
+  writeData(filtered);
   res.json({ success: true });
 });
 
-app.listen(3000, () => console.log("Backend běží na http://localhost:3000"));
+app.listen(3000, () => {
+  console.log("Backend běží na http://localhost:3000");
+});
